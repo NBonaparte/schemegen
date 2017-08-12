e0fa71aac91b0ee80ae1b0158dd2fb639f394f81
NBonaparte <98007b33@opayq.com>
2017-08-11 17:25:22 -0700

    get colors in 30 degree hue rotations

--- py/grapeharmony.py
+++ py/grapeharmony.py
@@ -6,6 +6,18 @@ def getAll(rgb):
     #new_colors = harmony.get_triad(rgb)
     return new_colors
 
+def getLoop(rgb):
+    colors = []
+    for i in range(0, 6):
+        colors.append(harmony.rotate(rgb, 60*i))
+    return colors
+
+def getMoreLoop(rgb):
+    colors = []
+    for i in range(0, 12):
+        colors.append(harmony.rotate(rgb, 30*i))
+    return colors
+
 def dist_key(a):
     return ca.euclidean_dist(a[0], ca.canon_od.get(a[1]))
 def test_full(path):
@@ -18,19 +30,23 @@ def test_full(path):
     xcols = ca.get_xcolors(cols)
     labeled = list(zip((ca.euclidean_dist(i, j) for i, j in zip(xcols, ca.canon_od.values())), xcols, ca.canon_od.keys()))
     #labeled = list(zip((harmony.hue_dist(i, j) for i, j in zip(xcols, ca.canon_od.values())), xcols, ca.canon_od.keys()))
-    #print(labeled)
     labeled.sort()
     # find the color that is closest to its canonical value which is not neutral
     labeled = [i for i in labeled if i[2] not in ["black", "darkgray", "lightgray", "white"]]
     print("picked " + labeled[0][2] + " to harmonize with: " + "#%02x%02x%02x" % labeled[0][1])
-    harmonized = getAll(labeled[0][1])
+    #print("2nd " + labeled[1][2] + " to harmonize with: " + "#%02x%02x%02x" % labeled[1][1])
+    #harmonized = getAll(labeled[0][1])
+    harmonized = getMoreLoop(labeled[0][1])
+    #harmonized = getLoop(labeled[1][1])
     for i in harmonized:
         rgb_to_hex(i)
     print()
-    #print(harmonized)
     combined = [i[1] for i in labeled] + list(harmonized)
     # add saturation filter?
-    xcol_final = ca.get_xcolors(combined)
+    redux = [xcols[0], xcols[-1]]
+    redux[1:1] = list(harmonized)
+    #xcol_final = ca.get_xcolors(combined)
+    xcol_final = ca.get_xcolors(redux)
     for i in xcol_final:
         rgb_to_hex(i)
     return xcol_final
